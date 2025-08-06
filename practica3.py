f9d965e7113a43b8d8f6d3cf05055ed6dad9e6ed
Manuel Gomez <manuel.gomez@cecyteo.edu.mnx>
2025-08-06 13:10:59 -0600

    Nuevas prácticas

---
+++ practica3.py
@@ -0,0 +1,8 @@
+import funciones
+
+if __name__ == "__main__":
+    numero1 = int(input("Ingresa número 1:"))
+    numero2 = int(input("Ingresa número 2:"))
+    print("La suma es:", funciones.sumar(numero1, numero2))
+
+print("Fin del programa.")
